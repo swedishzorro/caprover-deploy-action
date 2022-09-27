2fba368ae55f152c5b466ba2440f6b13449da875
Cristian Suazo <cris.suazo@gmail.com>
2022-09-28 00:17:27 +0200

    fix: tags not fetched

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 git config --global --add safe.directory /github/workspace
+git fetch --tags origin
 git archive --format tar --output deploy.tar $INPUT_REF
 caprover deploy -h $INPUT_SERVER -p $INPUT_PASSWORD -a $INPUT_APPNAME -t deploy.tar
