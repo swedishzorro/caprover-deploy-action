3240b1352ba290f5ff381d208862c0a7d8d8bb60
Cristian Suazo <cris.suazo@gmail.com>
2022-09-27 23:39:43 +0200

    fix: permission problem with git workspace

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
+git config --global --add safe.directory /github/workspace
 git archive --format tar --output deploy.tar $INPUT_REF
 caprover deploy -h $INPUT_SERVER -p $INPUT_PASSWORD -b $INPUT_BRANCH -a $INPUT_APPNAME -t deploy.tar
